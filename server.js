54e71baae9db309d7703454de439a6fd18e4c718
Karthik Rao Nagulapally <knagulapally@Karthik-LH.local>
2018-09-28 20:36:18 +0530

    Added GET Accounts API

--- server.js
+++ server.js
@@ -81,6 +81,42 @@ app.get('/api/toExtension', function (req, res) {
 
 });
 
+//get a list of accounts.
+app.get('api/accounts', function (req, res) {
+
+    req.session.originalURLPath = req.path;
+    // if auth has not been set, redirect to index
+    if (!req.session.accessToken || !req.session.instanceUrl) {
+
+        res.redirect('/auth/login');
+    }
+
+    //SOQL query
+    let q = 'SELECT id, name FROM account LIMIT 10';
+
+    //instantiate connection
+    let conn = new jsforce.Connection({
+        oauth2: { oauth2 },
+        accessToken: req.session.accessToken,
+        instanceUrl: req.session.instanceUrl
+    });
+
+    //set records array
+    let records = [];
+    let query = conn.query(q)
+        .on("record", function (record) {
+            records.push(record);
+        })
+        .on("end", function () {
+            console.log("total in database : " + query.totalSize);
+            console.log("total fetched : " + query.totalFetched);
+            res.json(records);
+        })
+        .on("error", function (err) {
+            console.error(err);
+        })
+        .run({ autoFetch: true, maxFetch: 4000 });
+});
 
 
 //get a list of accounts.
